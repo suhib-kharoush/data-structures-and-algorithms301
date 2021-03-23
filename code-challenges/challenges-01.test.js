8da86a048ca9434471f1ba453d9b314be5c55694
suhib-kharoush <suhaib_kharwash@outlook.com>
2021-03-23 22:47:39 +0200

    challenge complete

--- code-challenges/challenges-01.test.js
+++ code-challenges/challenges-01.test.js
@@ -44,6 +44,11 @@ Use `forEach` to loop over the input array. The modified strings should each be 
 
 const allUpperCase = (arr) => {
   // Solution code here...
+  const stringArr = [];
+  arr.forEach((Element) => {
+    stringArr.push(Element.toUpperCase())
+  });
+  return stringArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -58,10 +63,16 @@ Use `forEach` to build a new array of strings, each string modified by the callb
 
 const greeting = (word) => {
   // Solution code here...
+  return word.toUpperCase() + "!"
 };
 
 const speaker = (words, callback) => {
   // Solution code here...
+  const speakerArr = [];
+  words.forEach((Element) => {
+    speakerArr.push(callback(Element));
+  })
+  return speakerArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -82,10 +93,15 @@ Return the modified array.
 
 const addValues = (arr, value) => {
   // Solution code here...
+  arr.push(value);
 };
 
 const addNumbers = (num, arr, times, callback) => {
   // Solution code here...
+  for (let i = 0; i < times; i++) {
+    callback(arr, num)
+  }
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -108,6 +124,20 @@ This function should use forEach to populate your grocery list based on the stor
 
 const createList = (availableItems) => {
   // Solution code here...
+  availableItems = [
+    { name: 'apples', available: true },
+    { name: 'pears', available: true },
+    { name: 'oranges', available: false },
+    { name: 'bananas', available: true },
+    { name: 'blueberries', available: false }
+  ];
+  const inventory = [];
+  availableItems.forEach(Element => {
+    if (Element.available == true){
+      inventory.push(Element.name)
+    }
+  });
+  return inventory;
 };
 
 /* ------------------------------------------------------------------------------------------------
